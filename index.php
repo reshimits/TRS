731bb368b734735698cad4cab1c6e00754d48f3b
reshimits <Shimits@unknown002608e3426e.gateway.2wire.net>
2013-10-02 12:19:56 -0400

    Initial Commit
    Initial Push to GitHub

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>The Retrofit Source | Innovative Headlight Upgrades</title>
+	<meta name="description" content="Easily the biggest and most trusted distributor for automotive headlight retrofits with high-end xenon and bi-xenon projector systems in North America. And now with global reach, TRS is a recognized leader in HID headlight retrofitting.">
+	<meta name="author" content="THROTTLE, LTD. is a Columbus Ohio based boutique graphic design firm. We consult, strategize, design, build and manage web site, brand identity and print collateral projects for SME businesses. www.throttlestudios.com">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="format-detection" content="telephone=no" />
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/layout-home.css">
+
+	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+	<!-- Javascript [primary]
+  ================================================== -->
+  	<script type="text/javascript" src="javascript/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.flexslider.js"></script>	
+    <script type="text/javascript" charset="utf-8">
+		$(window).load(function() {
+					$('.flexslider').flexslider();
+				});
+				
+		function MM_swapImgRestore() { //v3.0
+		  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+		}
+		function MM_preloadImages() { //v3.0
+		  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+			var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+			if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+		}
+		
+		function MM_findObj(n, d) { //v4.01
+		  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+			d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+		  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+		  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+		  if(!x && d.getElementById) x=d.getElementById(n); return x;
+		}
+		
+		function MM_swapImage() { //v3.0
+		  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+		   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+		}
+	</script>
+    
+	<!-- Favicons
+  ================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	
+
+</head>
+<body onLoad="MM_preloadImages('images/carousel/carouselArrows-hover2x.png','images/btn_bg-orangeDark.png','images/btn_bg-blueDark.png','images/btn_bg-grayDark.png')">
+
+
+
+	<!-- Home Page Layout
+	================================================== -->
+
+ 
+    <?php require('include/header.html');?>
+    <div class="navMain hide460"><?php require('include/navMain.html');?></div>
+    <div class="navMobile mobile-only"><?php require('include/navMobile.html');?></div>
+    
+    	<div class="featureCarouselMain hide460"><?php require('include/featureCarouselMain.html');?></div>
+        <div class="featureCarouselMobile mobile-only"><?php require('include/featureCarouselMobile.html');?></div>
+        
+        
+        <div class="container" id="homePromos">
+            <div class="full column">
+            <div class="homePromosContainer">
+                <div class="homePromosL">            		
+                    <div class="homePromos-content">
+                        <span class="homePromos-title"><span class="hide300">The Almighty</span> Inspiration Gallery</span>
+                        <span class="homePromos-info">Customer projects in just about every imaginable (legal) application.</span>
+                        <div class="generalBtn-blue"><a href="#"><span class="generalBtn-title">Be Inspired</span></a></div> 
+                	</div>        
+                </div>
+                <div class="homePromosR"> 
+                    <div class="rRetrofitters">            		
+                        <div class="homePromos-content">
+                            <span class="homePromos-title">Recommended Retrofitters</span>
+                            <span class="homePromos-info">Find an approved professional to get the job done for you.</span>
+                            <div class="generalBtn-blue"><a href="/client/trs/retrofitters/recommended-retrofitters.php"><span class="generalBtn-title">Find an Installer</span></a></div> 
+                        </div>
+                    </div>
+                    <div class="diyInstallGuides">            		
+                        <div class="homePromos-content">
+                            <span class="homePromos-title">Do-It-Yourself Install Guides</span>
+                            <span class="homePromos-info">Step-by-step video and text instruction. Suhweeet!</span>
+                            <div class="generalBtn-blue"><a href="#"><span class="generalBtn-title">Get Busy</span></a></div> 
+                        </div>
+                    </div>
+        		</div>
+            </div>
+            </div>        
+        </div>
+        
+        <div class="container" id="homeAnnouncements">
+        	<div class="full column">
+        	<div class="homeAnnouncementsContainer">
+                <div class="homeAnnouncements-intro">            		
+                	<span class="homeAnnouncements-title">Promotions &amp; Announcements</span>
+                	<span class="homeAnnouncements-info"><span class="weightRegular-styleNormal">There’s always something new to know about over here at TRS.</span> 
+                    									 <br>Sales, new products, belching contest results. You know... that sort of thing.</span>
+                </div>
+                <div class="homeAnnouncements-newsContainer"> 
+                    <div class="homeAnnouncements-news news-mostRecent">            		
+                    	<span class="homeAnnouncements-news-title">The Sun Sure is Setting Early Lately!</span>
+                    	<span class="homeAnnouncements-news-info">Consider a Retrofit! All Stage III and IV Systems Include Free Shipping!</span>
+                    	<span class="homeAnnouncements-news-link"><a href="#">Shop Stage III</a></span>
+                    	<span class="homeAnnouncements-news-link"><a href="#">Shop Stage IV</a></span>
+                    </div>
+                    <div class="homeAnnouncements-news news-second">            		
+                    	<span class="homeAnnouncements-news-title">Did you know? </span>
+                    	<span class="homeAnnouncements-news-info">TRS is the official distributor for all Morimoto retrofitting parts. Nobody beats our selection and price.</span>
+                    	<span class="homeAnnouncements-news-link"><a href="#">Shop Morimoto</a></span>
+                    </div>
+                    <div class="homeAnnouncements-news news-third">            		
+                    	<span class="homeAnnouncements-news-title">New Arrival!</span>
+                    	<span class="homeAnnouncements-news-info">Black-Series Clear Lens Upgrade now available for most new Japanese vehicles with stock projectors!</span>
+                    	<span class="homeAnnouncements-news-link"><a href="#">Shop Black Series</a></span>
+                    </div>
+        		</div>
+            </div> 
+            </div>       
+        </div>
+        
+        <div class="container" id="homeFeatureProducts">
+        	<div class="full column">
+        	<div class="homeFeatureProductsContainer">
+                <div class="homeFeatureProductsL">
+                	<div class="homeFeatureProduct-wide homeFeatureProduct-1">
+                    <a href="#">            		
+                		<div class="homeFeatureProduct-product"><img src="/client/trs/images/products/productTemp1.jpg" alt="temp" border="0" /></div>
+                        <div class="homeFeatureProduct-content">
+                        	<span class="homeFeatureProduct-title">Morimoto D2S Elite HID System</span>
+                			<span class="homeFeatureProduct-price">$160</span>
+                        </div> 
+                        <div class="homeFeatureProduct-banner"><img src="/client/trs/images/products/banner_featureProduct-topSeller2x.png" alt="Top Selling Product!" border="0" /></div>
+                	</a>
+                    </div>
+                    <div class="homeFeatureProduct-wide homeFeatureProduct-2">            		
+                	<a href="#">
+                    	<div class="homeFeatureProduct-product"><img src="/client/trs/images/products/productTemp2.jpg" alt="temp" border="0" /></div>
+                        <div class="homeFeatureProduct-content">
+                        	<span class="homeFeatureProduct-title">Morimoto Mini H1 Stage III Complete Retrofit Kit</span>
+                			<span class="homeFeatureProduct-priceX">$270</span>
+                            <span class="homeFeatureProduct-price">$250</span>
+                        </div> 
+                        <div class="homeFeatureProduct-banner"><img src="/client/trs/images/products/banner_featureProduct-onSale2x.png" alt="Product On Sale!" border="0" /></div>
+                	</a>
+                    </div>
+                </div>
+                <div class="homeFeatureProductsR">
+                	<div class="homeFeatureProduct-tall homeFeatureProduct-3">            		
+                	<a href="#">
+                    	<div class="homeFeatureProduct-product"><img src="/client/trs/images/products/productTemp3.jpg" alt="temp" border="0" /></div>
+                        <div class="homeFeatureProduct-content">
+                        	<span class="homeFeatureProduct-title">Bi-Xenon Acura TL Projectors</span>
+                			<span class="homeFeatureProduct-price">$205</span>
+                        </div> 
+                        <div class="homeFeatureProduct-banner"><img src="/client/trs/images/products/banner_featureProduct-topSeller2x.png" alt="Top Selling Product!" border="0" /></div>
+                	</a>
+                    </div>
+                    <div class="homeFeatureProduct-tall homeFeatureProduct-4">            		
+                	<a href="#">
+                    	<div class="homeFeatureProduct-product"><img src="/client/trs/images/products/productTemp4.jpg" alt="temp" border="0" /></div>
+                        <div class="homeFeatureProduct-content">
+                        	<span class="homeFeatureProduct-title">Morimoto Xengreen D4S Bulbs</span>
+                            <span class="homeFeatureProduct-price">$60</span>
+                        </div> 
+                        <div class="homeFeatureProduct-banner"><img src="/client/trs/images/products/banner_featureProduct-new2x.png" alt="New Product!" border="0" /></div>
+                	</a>
+                    </div>
+                </div>
+            </div>
+            </div>        
+        </div>
+    	
+        <div class="container" id="homeTestimonials">
+        	<div class="full column">
+        	<div class="homeTestimonialsContainer">
+                <div class="homeTestimonials-intro">            		
+                	<span class="homeTestimonials-title">The Retrofit Source Experience</span>
+                	<span class="homeTestimonials-info">Without exception, we’ve dedicated ourselves to the art of the Happy Customer. <span class="mobile-hide"><br></span>We’re honored to have received affirmation with some pretty kind words...</span>
+                </div>
+                <div class="homeTestimonials-postContainer"> 
+                    <div class="homeTestimonials-post">            		
+                    	<span class="homeTestimonials-post-date">08.03.13</span>
+                    	<span class="homeTestimonials-post-quote">“I ordered an elite HID kit for my 2008 Nissan Altima. It doesn't have projectors so I just upgraded the bulbs. I am very pleased with the kit's quality and the product in general. Top notch lights and awesome customer service that cannot be beat! Thanks so much.”</span>
+                    	<span class="homeTestimonials-post-user">– Mike Fallon</span>
+                    </div>
+                    <div class="homeTestimonials-post">            		
+                    	<span class="homeTestimonials-post-date">07.24.13</span>
+                    	<span class="homeTestimonials-post-quote">“I have been coming to TRS ever since I did my first retro! I heard about the forum and later learned about the store. I bought my very first kit a while back, it was the Stage III FXR. I got it installed and ran with that for a while, got a new truck and came right back to TRS for more parts. I send all my friends here for all their HID, retro, and lighting needs! TRS should be your only source for lighting!”</span>
+                    	<span class="homeTestimonials-post-user">– Alex Vigneron</span>
+                    </div>
+                    <div class="homeTestimonials-post">            		
+                    	<span class="homeTestimonials-post-date">07.17.13</span>
+                    	<span class="homeTestimonials-post-quote">“I have been there and done that with PNP HID kits in my past vehicles and have recently "seen the light!" I was able to acquire all of the goodies needed from TRS to retrofit the headlights on my 08' Highlander. Their products are great and they stand behind them 100%! The tech support is amazing as well, they are just a phone call or email away and are willing to answer even the most ridiculous of questions! I will definitely be a customer with them for life and will be ordering again soon since my dad now wants to retro his motorcycle.”</span>
+                    	<span class="homeTestimonials-post-user">– Ryan Fanger</span>
+                    </div>
+        		</div>
+                <div class="showMore">
+                	<div class="generalBtn-blue"><a href="#"><span class="generalBtn-title">Show More</span></a></div> 
+                </div>
+            </div>
+            </div>        
+        </div>
+        
+        
+    
+	<?php require('include/footer.html');?>
+        
+    
+    
+
+	<!-- Javascript [secondary]
+  ================================================== -->
+  
+  
+<!-- End Document
+================================================== -->
+</body>
+
+</html>
