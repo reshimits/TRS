1e95dd4ed9cb706ac1e5d0b638852d33f998b6ab
Rob Shimits | reshimits <r.e@shimits.com>
2014-11-24 13:59:52 -0500

    2014 BlackFriday eNewsletter

---
+++ eNewsletters/2014-blackfriday/index.php
@@ -0,0 +1,154 @@
+<!doctype html> 
+
+<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><!--<![endif]-->
+<!--[if lt IE 7 ]><html dir="ltr" lang="en" class="ie6"><!--<![endif]-->
+<!--[if IE 7 ]><html dir="ltr" lang="en" class="ie7"><!--<![endif]-->
+<!--[if IE 8 ]><html dir="ltr" lang="en" class="ie8"><!--<![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]>   <html dir="ltr" lang="en" class="ie9"><!--<![endif]--><head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>The Retrofit Source | 2014 Thanksgiving deals running this Thursday through Black Friday and Cyber Monday</title>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="format-detection" content="telephone=no" />
+    
+    <!-- Stylesheets -->
+    <link type="text/css" href="css/styles-home.css" rel="stylesheet"  />
+
+    <!-- Javascript [primary]
+    ================================================== -->
+    <script type="text/javascript" src="javascript/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="//use.typekit.net/xib0jgf.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+</head>
+
+    
+		
+
+    <div class="topBackground">		
+        <div class="container" id="content">
+        	<div class="sixteen columns top clearfix">
+            	<span class="ensureNote">To ensure you receive our future emails, please add <span class="boldSpan">answers@theretrofitsource.com</span> to your address book.</span>
+        	</div>
+        </div>
+    </div>
+        
+    <div class="contentBackground">		
+        <div class="container" id="content">
+        	<div class="sixteen columns content clearfix">
+            
+            	<div class="headerContainer">
+                    <div class="logo">
+                        <a href="http://www.theretrofitsource.com/">
+                            <img src="images/logo2x.png" alt="TRS Logo" border="0" />
+                            <span class="logotype">THE RETROFIT SOURCE</span>
+                            <span class="logotagline">Innovative Headlight Upgrades</span>
+                        </a>
+                    </div>
+
+                    <div class="shop" id="global-hide480">
+                    	<a href="http://www.theretrofitsource.com/">
+                            <span class="shopText">Shop TRS</span>
+                            <img src="images/arrow-cta2x.png" alt="Shop TRS">
+                        </a>
+                    </div>
+                </div>        		
+                
+                <div class="feature" id="feature-main">
+                    <h1>The Legend Continues</h1>
+                    <h2>TRS Black Friday Sale is back for 2014!</h2>
+                    <span class="feature-subHeadline">Sale <span class="orangeSpanLight">Starts NOW</span> and <span class="orangeSpanLight">ends Cyber Monday at 9PM</span> EST</span>
+                    <div class="generalBtn-outer"><div class="generalBtn-orange"><a href="http://www.theretrofitsource.com/"><span class="generalBtn-title">Shop www.theretrofitsource.com now!</span></a></div></div>
+                </div>
+
+                <div class="gridDiscounts">
+                    <div class="discount">
+                        <span class="discountTitle">10%</span>
+                        <span class="discountCopy">Discount when you spend $100 or more, use Promo code:</span>
+                        <span class="discountCode">TRSGIVING10</span>
+                    </div>
+                    <div class="discount second-child">
+                        <span class="discountTitle">15%</span>
+                        <span class="discountCopy">Discount when you spend $150 or more, use Promo code:</span>
+                        <span class="discountCode">TRSGIVING15</span>
+                    </div>
+                    <div class="discount">
+                        <span class="discountTitle">20%</span>
+                        <span class="discountCopy">Discount when you spend $200 or more, use Promo code:</span>
+                        <span class="discountCode">TRSGIVING20</span>
+                    </div>
+                    <div class="discount last-child">
+                        <span class="discountTitle">25%</span>
+                        <span class="discountCopy">Discount when you spend $300 or more, use Promo code:</span>
+                        <span class="discountCode">TRSGIVING25</span>
+                    </div>
+                </div>
+                
+                <div class="gradientHeadline">
+                    <div class="stars"><img src="images/stars2x.png" alt="Stars" border="0" /></div>
+                    <div class="gradientHeadlineTitle">
+                        <span class="gradientHeadlineTitle-top">Combine deals &amp; Save</span><br>
+                        <span class="gradientHeadlineTitle-bottom">even more with these specially discounted items below!</span>
+                    </div>
+                    <div class="stars stars-right"><img src="images/stars2x.png" alt="Stars" border="0" /></div>
+                </div>        
+                
+                <div class="promos">    
+                    <a class="promo" id="promo1" href="http://www.theretrofitsource.com/components/projectors.html">
+                        <span class="featTitle"><span class="orangeSpan">Projectors:</span></br>Let there be light!</span>
+                        <span class="featCopy">Morimoto Mini H1 6.0 Bi-xenon: <span class="whiteSpan">$90</span></br>
+                        Morimoto Mini D2S 3.0 Bi-xenon: <span class="whiteSpan">$110</span></br>
+                        FX-R 3.0 Bi-xenon: <span class="whiteSpan">$90</span></br>
+                        Standard Mini H1 Bi-xenon (in closeouts): <span class="whiteSpan">$20</span></span>
+                    </a>
+                    <a class="promo" id="promo2" href="http://www.theretrofitsource.com/complete-retrofit-kits.html">
+                        <span class="featTitle"><span class="orangeSpan">Complete Kits:</span></br>Fill up!</span>
+                        <span class="featCopy">Select Morimoto Elite HID Systems! <span class="whiteSpan">$125</span></br>
+                        Bi-xenon Morimoto Mini H1 Stage III: <span class="whiteSpan">$270</span></br>
+                        Bi-xenon Morimoto Mini D2S Stage III: <span class="whiteSpan">$315</span></br>
+                        Bi-xenon FX-R Stage II: <span class="whiteSpan">$300</span></span>
+                    </a>
+                    <div class="promo" id="promo3">
+                        <span class="featTitle"><span class="orangeSpan">Retro Gear:</span></br>Add some awesome!</span>
+                        <span class="featCopy">Morimoto 3Five D2S Bulbs: <span class="whiteSpan">$25</span></br>
+                        Morimoto XB LED Projector Fog Lights: <span class="whiteSpan">$150</span></br>
+                        Morimoto XC LED RGB Halos: <span class="whiteSpan">$75</span></br>
+                        Morimoto XB35 and XB55 Ballasts: <span class="whiteSpan">$100</span></br>
+                        Osram D2S CBI Bulbs: <span class="whiteSpan">$185</span></br>
+                        JW Speaker Bi-LED Headlights: <span class="whiteSpan">$702-$1046</span></br>
+                        MDX-R Shrouds (closeouts!): <span class="whiteSpan">Free with purchase (limit 1 set)</span></span>
+                    </div>
+                    <div class="promo" id="promo4">
+                        <span class="featTitle"><span class="orangeSpan">Free vinyl stickers</span></br>with every order!</span>
+                        <span class="featTitle">24 Hour</br><span class="orangeSpan">In Stock Guarantee </span></br>on all Parts!</span>
+                        <span class="featCopy"><span class="whiteSpan">Customer service &amp; shipping department will re-open on Cyber Monday</span></span>
+                        <span class="featCopy"><span class="whiteSpan">*Sale excludes certain OEM projectors and wholesale orders, but that’s about it!*</span></span>
+                    </div>
+                </div> 
+                
+                <div class="subContent">
+                    <span class="subContent-note">Sit back, stuff yourself, and Light up your Black Friday with TRS!</span>
+                </div>
+
+                <div class="footerContainer"> 
+                	<img src="images/trs-badge2x.png" alt="TRS Badge">
+                    <span class="footerCopy1">Enjoy the fresh deals and turkey!</span>
+                    <span class="footerCopy2">Get more info at <a href="http://www.theretrofitsource.com/">www.theretrofitsource.com</a> or <a href="http://www.theretrofitsource.com/contact_us.php">contact us</a></span>
+                	<span class="footerCopy3"><a href="tel:404-220-7940" class="tel">404.220.7940</a></span>
+                </div> 
+                 
+                                   
+        	</div>
+        </div>
+    </div>
+    
+       
+    
+    
+    
+
+</body>
+
+</html>
